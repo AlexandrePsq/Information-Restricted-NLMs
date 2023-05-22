ad3a9842499f1b4ce14c98b1bedffa6d6bffd36a
AlexandrePsq <alexandre-pasquiou@orange.fr>
2023-05-22 11:29:36 +0200

    fix function input

--- src/irnlm/data/extract_semantic_features.py
+++ src/irnlm/data/extract_semantic_features.py
@@ -7,19 +7,19 @@ from irnlm.data.utils import (
     get_positions_words,
     get_quantity_words,
     get_function_words,
-    get_punctuation
+    get_punctuation,
 )
 from irnlm.data.text_tokenizer import tokenize
 
 
 def get_mapping(language):
-    """
-    """
-    mapping = {word: 'PRON' for word in get_pronouns(language)}
-    mapping.update({word: 'POSITION' for word in get_positions_words(language)})
-    mapping.update({word: 'NEG' for word in get_negations(language)})
-    mapping.update({word: 'QUANTITY' for word in get_quantity_words(language)})
+    """ """
+    mapping = {word: "PRON" for word in get_pronouns(language)}
+    mapping.update({word: "POSITION" for word in get_positions_words(language)})
+    mapping.update({word: "NEG" for word in get_negations(language)})
+    mapping.update({word: "QUANTITY" for word in get_quantity_words(language)})
     return mapping
+
 
 def clean_sentence(sent, mapping, function_words):
     """Replace pronouns with PRON.
@@ -28,12 +28,16 @@ def clean_sentence(sent, mapping, function_words):
     Replace quantity with QUANTITY.
     Remove function words.
     """
-    words = sent.split(' ')
-    words  = [mapping[word.lower()] if word.lower() in mapping.keys() else word for word in words]
-    sent = ' '.join([word for word in words if word.lower() not in function_words])
+    words = sent.split(" ")
+    words = [
+        mapping[word.lower()] if word.lower() in mapping.keys() else word
+        for word in words
+    ]
+    sent = " ".join([word for word in words if word.lower() not in function_words])
     return sent
 
-def integral2semantic(path, language='english', n_jobs=-1, convert_numbers=False):
+
+def integral2semantic(path, language="english", n_jobs=-1, convert_numbers=False):
     """Extract semantic features from the integral text.
     Args:
         - path: path
@@ -44,13 +48,15 @@ def integral2semantic(path, language='english', n_jobs=-1, convert_numbers=False
     """
     mapping = get_mapping(language)
     function_words = get_function_words(language=language) + get_punctuation()
-    iterator = tokenize(path, language=language, with_punctuation=True, convert_numbers=convert_numbers)
+    iterator = tokenize(
+        path, language=language, with_punctuation=True, convert_numbers=convert_numbers
+    )
     iterator = [item.strip() for item in iterator]
-    iterator = Parallel(n_jobs=n_jobs, batch_size=1000)(
-        delayed(clean_sentence)(sent, mapping, function_words) 
-        for sent in tqdm(iterator, desc='Cleaning sentences', total=len(iterator))
-        )
-    #iterator = [clean_sentence(sent, mapping, function_words) for sent in tqdm(iterator, desc='Cleaning sentences', total=len(iterator))]
-    iterator = [item for item in iterator if item !='']
+    n = len(iterator)
+    iterator = Parallel(n_jobs=n_jobs, batch_size=min(1000, n))(
+        delayed(clean_sentence)(sent, mapping, function_words)
+        for sent in tqdm(iterator, desc="Cleaning sentences", total=n)
+    )
+    # iterator = [clean_sentence(sent, mapping, function_words) for sent in tqdm(iterator, desc='Cleaning sentences', total=len(iterator))]
+    iterator = [item for item in iterator if item != ""]
     return iterator
-
